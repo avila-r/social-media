73e18a44a90e3cf3270c3e527eb7979f08522c61
avila-r <avila.dev@outlook.com>
2024-06-17 22:47:38 -0300

    feat: implement registration DTOs

--- account/src/main/kotlin/com/avila/account/model/Register.kt
+++ account/src/main/kotlin/com/avila/account/model/Register.kt
@@ -1,9 +1,17 @@
 package com.avila.account.model
 
+import java.time.LocalDate
+
 data class RegisterRequest (
-    val name: String
+    val fullName: String,
+    val birthDate: LocalDate,
+    val gender: String,
+    val login: String,
+    val password: String,
+    val email: String,
 )
 
 data class RegisterResponse (
-    val name: String
+    val account: Account,
+    val profile: Profile
 )
