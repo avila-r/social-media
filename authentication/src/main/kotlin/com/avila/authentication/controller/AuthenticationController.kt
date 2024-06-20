ad862ed466a7ebb62fdab5869e59c64a7d5c1484
avila-r <avila.dev@outlook.com>
2024-06-19 23:06:10 -0300

    feat: implement /login handler

--- authentication/src/main/kotlin/com/avila/authentication/controller/AuthenticationController.kt
+++ authentication/src/main/kotlin/com/avila/authentication/controller/AuthenticationController.kt
@@ -1,13 +1,36 @@
 package com.avila.authentication.controller
 
-import org.springframework.web.bind.annotation.GetMapping
+import com.avila.authentication.exception.InvalidTokenGenerationException
+import com.avila.authentication.jwt.service.JsonWebTokenService
+import com.avila.authentication.model.Account
+import com.avila.authentication.model.LoginRequest
+import com.avila.authentication.model.LoginResponse
+
+import jakarta.validation.Valid
+
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/api/v1/auth")
-@RestController class AuthenticationController {
+@RestController class AuthenticationController ( private val manager: AuthenticationManager, private val service: JsonWebTokenService ) {
 
-    @GetMapping("/login")
-    fun login() = "Hello World"
+    @PostMapping("/login")
+    fun login(@Valid @RequestBody request: LoginRequest): LoginResponse {
+
+        val authentication = manager.authenticate(
+            UsernamePasswordAuthenticationToken(
+                request.login,
+                request.password
+            )
+        )
+
+        return LoginResponse (
+            service.generate(authentication.principal as Account) ?: throw InvalidTokenGenerationException("Invalid login submit"), authentication
+        )
+    }
 
 }
