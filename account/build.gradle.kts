a4a00b741b29ee4e98ab4cf01d9ca860fdb68d11
avila-r <avila.dev@outlook.com>
2024-06-20 23:34:35 -0300

    build: update dependencies

--- account/build.gradle.kts
+++ account/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
      */
 		implementation("org.springframework.boot:spring-boot-starter-web")
 		implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+		implementation("org.springframework.boot:spring-boot-starter-validation")
+		implementation("org.springframework.boot:spring-boot-starter-security")
 
 		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 		implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -50,6 +52,11 @@ dependencies {
      * PostgreSQL
      */
 		implementation("org.postgresql:postgresql:42.7.3")
+
+	/*
+     * Docs
+     */
+		implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
 	/*
      * Tests
