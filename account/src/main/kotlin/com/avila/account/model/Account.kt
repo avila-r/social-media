7a80ca2c16fb4cf5d6ce1c5320cd0d50f12d1c61
avila-r <avila.dev@outlook.com>
2024-06-20 22:43:34 -0300

    feat: implement validation constraints

--- account/src/main/kotlin/com/avila/account/model/Account.kt
+++ account/src/main/kotlin/com/avila/account/model/Account.kt
@@ -3,41 +3,48 @@ package com.avila.account.model
 import jakarta.persistence.Table
 import jakarta.persistence.Entity
 import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Table(name = "accounts")
+@EntityListeners(AuditingEntityListener::class)
 @Entity class Account (
 
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @field:Column(name = "id", unique = true, nullable = false)
+    @Id val id: UUID? = null,
+
+    @field:Column(name = "created_at", nullable = false, updatable = false)
+    @field:CreatedDate
+    val createdAt: LocalDateTime? = null,
+
     @Column(name = "login", unique = true, nullable = false)
+    @field:NotBlank
     val login: String,
 
     @Column(name = "password_hash", nullable = false)
+    @field:Size(min = 8)
     val password: String,
 
     @Column(name = "email", unique = true, nullable = false)
+    @field:Email
     val email: String,
 
-    ) {
+    @field:Column(name = "status", nullable = false)
+    val status: String? = null,
 
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "id", unique = true, nullable = false)
-    @Id var id: UUID? = null
+    @field:Column(name = "last_login_at", nullable = true)
+    val lastLoginAt: LocalDateTime? = null
 
-    @Column(name = "created_at", nullable = false)
-    @CreatedDate
-    var createdAt: LocalDateTime? = null
-
-    @Column(name = "status", nullable = false)
-    var status: String? = null
-
-    @Column(name = "last_login_at", nullable = true)
-    var lastLoginAt: LocalDateTime? = null
-
-}
+)
--- account/src/main/kotlin/com/avila/account/model/Register.kt
+++ account/src/main/kotlin/com/avila/account/model/Register.kt
@@ -1,13 +1,31 @@
 package com.avila.account.model
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Past
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+import jakarta.validation.constraints.Email
+
 import java.time.LocalDate
 
 data class RegisterRequest (
+    @field:NotBlank
     val fullName: String,
+
+    @field:Past
     val birthDate: LocalDate,
+
+    @field:Pattern(regexp = "^(?i)(male|female)\$")
     val gender: String,
+
+    @field:NotBlank
     val login: String,
+
+    @NotBlank
+    @field:Size(min = 8)
     val password: String,
+
+    @field:Email
     val email: String,
 )
 
