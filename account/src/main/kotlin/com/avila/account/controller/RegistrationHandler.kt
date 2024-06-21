a1f42cd8d73a07903fcdc0754f13757fc226b754
avila-r <avila.dev@outlook.com>
2024-06-20 22:46:41 -0300

    feat: validate request body at post mapping

--- account/src/main/kotlin/com/avila/account/controller/RegistrationHandler.kt
+++ account/src/main/kotlin/com/avila/account/controller/RegistrationHandler.kt
@@ -3,15 +3,18 @@ package com.avila.account.controller
 import com.avila.account.model.RegisterRequest
 import com.avila.account.service.RegistrationService
 
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/api/v1/register")
+@RequestMapping("/api/v1/auth/register")
 @RestController class RegistrationHandler ( private val service: RegistrationService ) : Handler {
 
     @PostMapping
-    fun register(@RequestBody request: RegisterRequest) = handle(service.register(request))
+    fun register(@Valid @RequestBody request: RegisterRequest) = handle(HttpStatus.CREATED, service.register(request))
 
 }
