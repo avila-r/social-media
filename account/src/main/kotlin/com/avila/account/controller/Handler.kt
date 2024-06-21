a1fcc80ac0d88a6a963f5ef306feda918320592f
avila-r <avila.dev@outlook.com>
2024-06-20 22:47:00 -0300

    feat: implement secundary handle() signature

--- account/src/main/kotlin/com/avila/account/controller/Handler.kt
+++ account/src/main/kotlin/com/avila/account/controller/Handler.kt
@@ -3,19 +3,22 @@ package com.avila.account.controller
 import com.avila.account.error.Error
 
 import com.github.michaelbull.result.*
+import org.springframework.http.HttpStatus
 
 import org.springframework.http.ResponseEntity
 
 interface Handler {
 
-    fun <V, E : Error, R : Result<V, E>> handle(r: R): Result<ResponseEntity<V>, ResponseEntity<E>>  =
+    fun <V, E : Error> handle(result: Result <V, E>) =
+        if (result.isErr)
+            ResponseEntity.status(result.error.status).body(result.error)
+        else
+            ResponseEntity.ok(result.value)
 
-        if (r.isErr) Err (
-            ResponseEntity.status(r.error.status).body(r.error)
-        )
-
-        else Ok (
-            ResponseEntity.ok(r.value)
-        )
+    fun <V, E : Error> handle(status: HttpStatus, result: Result<V, E>) =
+        if (result.isErr)
+            ResponseEntity.status(result.error.status).body(result.error)
+        else
+            ResponseEntity.status(status).body(result.value)
 
 }
